2f147cc3c25f6821b3eb25926bdc63e3376c9506
ImNotWhyLclc <qarahhanya@gmail.com>
2025-11-03 15:36:44 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,13 @@
 local base = "https://raw.githubusercontent.com/ImNotWhyLclc/Rendex/main/"
 local hm = {
-    [606849621] = "games/jailbreak-break.luau",
+--    [606849621] = "", -- soon
     [127742093697776] = "Games/RendexPVB.lua" -- pvb
 }
 local file = hm[game.PlaceId]
 local so, tuff = pcall(function()
     return loadstring(game:HttpGet(base..file))()
 end)
-if ok then
+if so then
     print("[Rendex] Loaded:", file)
 else
     warn("[Rendex] Failed:", tuff)
