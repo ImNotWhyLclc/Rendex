f35fa7b19c767291e86f3410c11dabaa5d2a8063
ImNotWhyLclc <qarahhanya@gmail.com>
2025-11-03 15:31:34 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,14 @@
+local base = "https://raw.githubusercontent.com/Breadido/Codecoat/main/"
+local hm = {
+    [606849621] = "games/jailbreak-break.luau",
+    [17516596118] = "games/hypershot.lua"
+}
+local file = hm[game.PlaceId]
+local so, tuff = pcall(function()
+    return loadstring(game:HttpGet(base..file))()
+end)
+if ok then
+    print("[Rendex] Loaded:", file)
+else
+    warn("[Rendex] Failed:", tuff)
+end
