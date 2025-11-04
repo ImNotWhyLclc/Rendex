98472ea67d9d5a39a7660c8a7bf586960df24ee4
ImNotWhyLclc <qarahhanya@gmail.com>
2025-11-04 11:34:53 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 local base = "https://raw.githubusercontent.com/ImNotWhyLclc/Rendex/main/"
 local hm = {
-    [606849621] = "Games/RendexJB.lua", -- jb
-    [127742093697776] = "Games/RendexPVB.lua" -- pvb
+    [606849621] = "Games/RendexJB.luau", -- jb
+    [127742093697776] = "Games/RendexPVB.luau" -- pvb
 }
 local file = hm[game.PlaceId]
 local so, tuff = pcall(function()
